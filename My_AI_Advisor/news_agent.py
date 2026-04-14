f630e3b68bfefbf67acf2448a10706e43dbaa841
Baek900 <qorrbrla1@gmail.com>
2026-04-14 11:23:35 +0900

    Update news_agent.py

--- My_AI_Advisor/news_agent.py
+++ My_AI_Advisor/news_agent.py
@@ -51,7 +51,7 @@ def get_news_analysis(market_name, query, cutoff_datetime_str, time_zone_str):
     )
 
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash", 
+        model="gemini-2.5-flash-light", 
         google_api_key=google_key,
         temperature=0.1
     )
